7f52e5717deecedbbd86c112ec0b167a58335356
17KryptoVos <17kryptovos@protonmail.com>
2020-07-31 15:33:48 +0200

    - Added logger to SportConfig.java

--- src/com/kryptovos/MyLoggerConfig.java
+++ src/com/kryptovos/MyLoggerConfig.java
@@ -1,29 +1,15 @@
 package com.kryptovos;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:mylogger.properties")
 public class MyLoggerConfig {
 
-    @Value("${root.logger.level}")
-    private String rootLoggerLevel;
-
-    @Value("${printed.logger.level}")
-    private String printedLoggerLevel;
-
-    @PostConstruct
-    public void initLogger() {
+    public MyLoggerConfig(String rootLoggerLevel, String printedLoggerLevel) {
 
         // parse levels
         Level rootLevel = Level.parse(rootLoggerLevel);
--- src/com/kryptovos/SportConfig.java
+++ src/com/kryptovos/SportConfig.java
@@ -1,12 +1,24 @@
 package com.kryptovos;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-// Not using component scan
-//@ComponentScan
+@PropertySource("classpath:mylogger.properties")
 public class SportConfig {
+
+    // Define logger
+    @Bean
+    public MyLoggerConfig myLoggerConfig(@Value("${root.logger.level}") String rootLoggerLevel,
+                                         @Value("${printed.logger.level}") String printedLoggerLevel) {
+
+        MyLoggerConfig myLoggerConfig = new MyLoggerConfig(rootLoggerLevel, printedLoggerLevel);
+
+        return myLoggerConfig;
+    }
+
 
     // Define Bean for Happy Fortune Service
     @Bean
